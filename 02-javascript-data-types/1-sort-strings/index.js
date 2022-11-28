4d824dd4ed863c1c7ce5f4e6c08e359db00bc701
AlexuL <alexulanad@gmail.com>
2022-11-28 17:59:26 +0300

    Исправления git...

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,7 +5,6 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-<<<<<<< HEAD
   const arrCopy = [...arr].sort( (a, b) => new Intl.Collator(["ru", "en-GB", "en-US"], { caseFirst: "upper" }).compare(a, b));
 =======
   // const arrCopy = [...arr];
@@ -14,6 +13,8 @@ export function sortStrings(arr, param = 'asc') {
   // }
   // return arrCopy.sort( (a, b) => new Intl.Collator(["ru", "en-GB", "en-US"], {caseFirst: "upper"}).compare(a, b));
   const arrCopy = [...arr].sort( (a, b) => new Intl.Collator(["ru", "en-GB", "en-US"], {caseFirst: "upper"}).compare(a, b));
->>>>>>> 3d3af561a64de592cc3249991debdad041306dc1
+  return param === "desc" ? arrCopy.reverse() : arrCopy;
+
+  const arrCopy = [...arr].sort( (a, b) => new Intl.Collator(["ru", "en-GB", "en-US"], { caseFirst: "upper" }).compare(a, b));
   return param === "desc" ? arrCopy.reverse() : arrCopy;
 }
