909f98640bae094da7f25f566f1b50c1cdd5b27a
AlexuL <alexulanad@gmail.com>
2022-12-07 20:07:14 +0300

    Внесены изменения в класс компонента: ColumnChart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,20 +1,27 @@
 export default class ColumnChart {
-  constructor(fields = {}) {
-    this.data = fields.data || [];
-    this.label = fields.label || '';
-    this.value = fields.value || 0;
-    this.link = fields.link || '';
-    this.formatHeading = fields.formatHeading || (data => `${this.value}`);
-    this.chartHeight = 50;
+  subElements = {};
+  chartHeight = 50;
+
+  constructor({
+    data = [],
+    label = "",
+    link = "",
+    value = 0,
+    formatHeading = data => data,
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = formatHeading(value);
 
     this.render();
-    this.initEventListeners();
   }
-  getColumnProps(data) {
-    const maxValue = Math.max(...data);
-    const scale = 50 / maxValue;
 
-    return data.map(item => {
+  getColumnProps() {
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data.map(item => {
       return {
         percent: (item / maxValue * 100).toFixed(0) + '%',
         value: String(Math.floor(item * scale))
@@ -22,34 +29,27 @@ export default class ColumnChart {
     });
   }
 
-  getDataList(data) {
-    return this.getColumnProps(data).map(item => {
+  getColumnBody() {
+    return this.getColumnProps().map(item => {
       return `<div style="--value: ${item.value}" data-tooltip="${item.percent}"></div>`;
     }).join('');
   }
 
   getLink() {
-    if (this.link) {
-      return `<a href="/${this.link}" class="column-chart__link">View all</a>`;
-    }
+    return this.link ? `<a href="/${this.link}" class="column-chart__link">View all</a>` : "";
   }
 
-  getTemplateLoading() {
-    if (this.data.length === 0) {return 'column-chart column-chart_loading';}
-    if (this.data.length > 0) {return 'column-chart';}
-  }
-
-  getTemplate() {
+  get template() {
     return `
-      <div class="${this.getTemplateLoading()}" style="--chart-height: 50">
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
       <div class="column-chart__title">
         ${this.label}
-        ${this.getLink() || ''}
+        ${this.getLink()}
       </div>
       <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
+        <div data-element="header" class="column-chart__header">${this.value}</div>
         <div data-element="body" class="column-chart__chart">
-          ${this.getDataList(this.data)}
+          ${this.getColumnBody()}
         </div>
       </div>
     </div>
@@ -57,25 +57,48 @@ export default class ColumnChart {
   }
 
   render() {
-    const element = document.createElement("div");
-    element.innerHTML = this.getTemplate();
-    this.element = element.firstElementChild;
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+
+    if (this.data.length) {
+      this.element.classList.remove("column-chart_loading");
+    }
+
+    this.subElements = this.getSubElements();
   }
 
-  initEventListeners() {
-    // NOTE: в данном методе добавляем обработчики событий, если они есть
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll("[data-element]");
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
   }
+
+
   update(data) {
-    const chart = this.element.querySelector('.column-chart__chart');
-    if (data.length !== 0) {chart.innerHTML = this.getDataList(data);}
+    if (!data.length) {
+      this.element.classList.add("column-chart_loading");
+    }
+
+    this.data = data;
+    this.subElements.body.innerHTML = this.getColumnBody();
   }
 
   remove() {
-    this.element.remove();
+    if (this.element) {
+      this.element.remove();
+    }
   }
 
   destroy() {
     this.remove();
+    this.element = {};
     // NOTE: удаляем обработчики событий, если они есть
   }
 }
