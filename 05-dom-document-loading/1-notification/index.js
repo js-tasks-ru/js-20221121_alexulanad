cc3a2ab3736f0e0a9d76e91eff93ef06478713bc
AlexuL <alexulanad@gmail.com>
2022-12-08 15:29:21 +0300

    Реализован компонент: NotificationMessage.

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,53 @@
 export default class NotificationMessage {
+  constructor(message = "No message", {
+    duration = 2000,
+    type = "success",
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render()
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration/1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+  }
+
+  show(target = document.body) {
+    target.append(this.element);
+
+    setTimeout( () => {
+      this.destroy();
+    }, this.duration - 10) // сокращаем время, чтобы убрать мерцание в момент удаления
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+
+    return this;
+  }
+
+  destroy() {
+    this.element.remove();
+    this.remove();
+  }
 }
